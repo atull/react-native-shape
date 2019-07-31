50f09b26d278ea723d5d6c9d24b2f8cdf7007747
Atul <atul@ktds.co>
2019-07-31 18:56:24 +0530

    circle style

--- src/Circle.js
+++ src/Circle.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
-export const Circle = () => (
-  <View style={{ width: 100, height: 100, backgroundColor: 'red' }} />
+export const Circle = ({ color:string = '#1e90ff' }) => (
+  <View style={[styles.shape, { backgroundColor: color }]} />
 );
+
+const styles = StyleSheet.create({
+  shape: {
+    width: 100,
+    height: 100,
+    borderRadius: 100/2,
+  },
+});
